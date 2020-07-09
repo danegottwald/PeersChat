03ac932585901ae09aaaf8656c085ca7ddabb6d5
Omar Ahmadyar <oahmadyar@gmail.com>
2020-07-09 13:02:38 -0700

    Added Documentation for PC_Network.hpp

--- src/PC_Network.hpp
+++ src/PC_Network.hpp
@@ -48,6 +48,19 @@
 // Audio Packet Struct ---------------------------------------------------------
 /* AudioPacket: A struct that contains an encoded audio packet
  *
+ * @member packet_id  Unique id for an incoming packet from this peer.  Packets
+ *                    received will be numbered sequentially starting with zero
+ *
+ * @member packet_len The size of packet in bytes
+ *
+ * @member packet  A unique pointer to a buffer meant for containing an encoded
+ *                 audio packet from libopus
+ *
+ * @constructor AudioPacket  Initializes packet to at least BUFFER_SIZE which is
+ *                           always at least 4096
+ *
+ * @operator <  Less than operator overload for comparing and sorting based
+ *              packet id number.
  */
 struct AudioPacket
 {
@@ -67,7 +80,73 @@ struct AudioOutPacket : public AudioPacket { };
 // NPeer Class -----------------------------------------------------------------
 /* NPeer: A class for handling networking to your peers
  *
- * TODO: Documentation
+ * @member udp  Shared UDP socket used to send data out to all peers.  Setting
+ *              this paremeter in any NPeer class sets it for all as per the
+ *              static keyword.
+ *
+ * @member tcp  TCP Socket used to communicate data to this specific peer.  Used
+ *              for data where guaranteed delivery is important... pretty much
+ *              everything except for audio.
+ *
+ * @member sockaddr_in  Destination address for this peer's UDP socket. Audio
+ *                      data sent over @udp will be sent to this address.
+ *
+ * @member in_packets  Priority queue of type @AudioInPacket ordered by
+ *                     @packet_id such that popping off an element will get you
+ *                     the lowest numbered packet.  This is used to get packets
+ *                     in order even if they arrived out of order.
+ *
+ * @member in_queue_lock  Mutex lock to ensure mutual exclusion with @in_packets
+ *
+ * @member in_bucket  Queue of type @AudioInPacket.  It's used to store packets
+ *                    that aren't currently in @in_packets.  The point of this
+ *                    is to store the unused packets instead of destructing and
+ *                    constructing them every time.
+ *
+ * @member in_bucket_lock  See @in_queue_lock but with @in_packets replaced with
+ *                         @in_bucket.
+ *
+ * @member in_packet_id  @packet_id from the last @AudioInPacket that was
+ *                       popped off @in_packets.  Used to identify if any
+ *                       packets were dropped.
+ *
+ * @member out_packets  Queue of type @AudioOutPacket.  Exists for the purpose
+ *                      of queuing up encoded audio to be sent over network to
+ *                      peer.
+ *
+ * @member out_queue_lock  See @in_queue_lock but for @out_packets instead of
+ *                         @in_packets.
+ *
+ * @member out_bucket  See @in_bucket but with @AudioOutPacket instead of
+ *                     @AudioInPacket and @out_packets instead of @in_packets.
+ *
+ * @member out_packet_id  Next id that will be assigned to @packet_id in
+ *                        @AudioPacket.  Handles sequentially numbering packets.
+ *
+ * @constructor NPeer(0)  Default constructor
+ *
+ * @constructor NPeer(2)  Constructor that initializes address structure for
+ *                        peer destination.
+ *
+ * @method getEmptyOutPacket()  Method that returns an @AudioOutPacket.  Packet
+ *                              may have junk/old data in it.  Make sure to
+ *                              overwrite.  @AudioOutPacket returned should be
+ *                              passed to enqueue_out after populated with
+ *                              being packet data.
+ *
+ * @method enqueue_out(1)  Enqueue's encoded audio packet in the form of
+ *                         @AudioOutPacket for the purposes of being sent to
+ *                         peer out @udp socket.
+ *
+ * @method getEmptyInPacket()  Method that returns an @AudioInPacket.  Packet
+ *                             contains an audio packet to be played out
+ *                             speaker.  @packet_id parameter in
+ *                             @AudioInPacket can be used to identify how many
+ *                             packets were dropped beforehand.
+ *
+ * @method enqueue_in(1)  Enqueue's an audio packet.  Get's queue'd into
+ *                        @in_packets Priority Queue so the lowest id packet
+ *                        gets popped first.
  */
 class NPeer
 {
@@ -87,7 +166,7 @@ private:
 		// Audio out
 	std::queue<std::unique_ptr<AudioOutPacket>> out_packets;
 	std::mutex out_queue_lock;
-	std::queue<std::unique_ptr<AudioOutPacket>> bucket;
+	std::queue<std::unique_ptr<AudioOutPacket>> out_bucket;
 	std::mutex out_bucket_lock;
 	uint32_t out_packet_id;
 
