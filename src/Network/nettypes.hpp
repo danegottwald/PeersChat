eda53190279350c0261e357cb5034e18a00cdd80
ivandiep <diep.ivan@yahoo.com>
2020-07-13 19:31:24 -0700

    created header defining net codes for handshake protocol

---
+++ src/Network/nettypes.hpp
@@ -0,0 +1,14 @@
+#ifndef _NETTYPES_HPP
+#define _NETTYPES_HPP
+
+enum NETCODES { CONNECT=0x1, 
+				SHARE=0x81, 
+				SENDP=0x82, 
+				SENDV=0x83, 
+				REQP=0x3, 
+				DENY=0x4, 
+				ACCEPT=0x84, 
+				PROPOSE=0x5
+}
+
+#endif
