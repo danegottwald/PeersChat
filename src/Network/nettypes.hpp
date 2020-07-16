40047f77e740cc16394081f964f9f7c2526442b0
Ivan <diep.ivan@yahoo.com>
2020-07-16 00:29:16 -0700

    Added documentation for each NETCODE request and added the DISCONNECT and CLOSE request type

--- src/Network/nettypes.hpp
+++ src/Network/nettypes.hpp
@@ -1,14 +1,24 @@
 #ifndef _NETTYPES_HPP
 #define _NETTYPES_HPP
 
-enum NETCODES { CONNECT=0x1,
-                SHARE=0x81,
-                SENDP=0x82,
-                SENDV=0x83,
-                REQP=0x3,
-                DENY=0x4,
-                ACCEPT=0x84,
-                PROPOSE=0x5
+/* NETCODES: An enum that contains all request= types. Requests are sent in
+ * a one byte. Request types that contain data will have their left most bit
+ * set to 1.
+ * 
+ * Format: [request type] [content length] [message...]
+ * 
+*/
+enum NETCODES { 
+                CONNECT=0x1,    // Request to connect to an existing call
+                SHARE=0x81,     // Share user info with host
+                SENDP=0x82,     // Share list of peers with joined user
+                SENDV=0x83,     // Send voice to other peers
+                REQP=0x3,       // Send voice to other peers
+                DENY=0x4,       // Deny request to join
+                ACCEPT=0x84,    // Accept request to join
+                PROPOSE=0x5,    // Ask current peers if new peer can join
+                DISCONNECT=0x6, // Disconnect from call; Leave server
+                CLOSE=0x7       // Close TCP pipe; end request
 };
 
 #endif
