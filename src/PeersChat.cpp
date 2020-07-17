03d8690089960d3c45dab3e345c8b11c5cb79184
Ivan <diep.ivan@yahoo.com>
2020-07-17 01:30:43 -0700

    Added basic command line parsing using getopt Using getopt.c and getopt.h from GNU C Library for compatibility with Windows

--- src/PeersChat.cpp
+++ src/PeersChat.cpp
@@ -1,8 +1,33 @@
-
-
+#include <iostream>
+#include <string>
+#include "getopt.h"
 
 int main(const int argc, char *argv[])
-{
-
-	return 0;
+{	
+	std::string user, address; // no flag for ip address
+	if(argc > 1)
+	{
+		int choice;
+		while((choice = getopt(argc,argv,"hu:")) != -1)
+		{
+			switch(choice)
+			{
+				case 'h': // host a room
+					break;
+				case 'u': // enter a username
+					user = optarg;
+					break;
+				default:
+					return EXIT_FAILURE;
+			}
+		}
+		address = argv[optind];
+		std::cout << address << std::endl;
+	}
+	else
+	{
+		std::cout << "GUI mode" << std::endl;
+	}
+	return EXIT_SUCCESS;
 }
+
