c3e2a0cd56d6628000e7c4b179d6659542a5f269
Ivan <diep.ivan@yahoo.com>
2020-07-19 04:01:02 -0700

    Added/Updated CLI flags and related functions. updated includes and class members added functions for leave, volume, and mute

--- src/PeersChat.cpp
+++ src/PeersChat.cpp
@@ -1,51 +1,78 @@
 #include <string>
 #include <unistd.h>
 #include "PC_Network.hpp"
-#include "PC_Audio.h"
+#include "PC_Audio.hpp"
 #include "PC_GUI.hpp"
 
 class PeersChat
 {
 	public:
-		NPeer network;
-		PC_AudioHandler audio;
+		NPeer peer;
+		PeersChatNetwork network;
+		APeer audio;
 		PC_GuiHandler GUI;
 		bool useGUI = true;
-		std::string user; 
-		std::string address; // no flag for ip address
 };
 
 int main(const int argc, char *argv[])
 {	
-	PeersChat peer;
+	PeersChat pchat;
 	if(argc > 1)
 	{
-		peer.useGUI = false;
+		pchat.useGUI = false;
 		int choice;
-		while((choice = getopt(argc,argv,"hu:")) != -1)
+		while((choice = getopt(argc,argv,"hj:u:")) != -1)
 		{
 			switch(choice)
 			{
 				case 'h': // host a room
+					pchat.network.host();
+					break;
+				case 'j': // join a room, need an address
+					pchat.network.join(optarg);
 					break;
 				case 'u': // enter a username
-					peer.user = optarg;
+					pchat.peer.setName(optarg);
 					break;
 				default:
 					return EXIT_FAILURE;
 			}
 		}
-		peer.address = argv[optind];
 
 		while(true)
 		{
-			// Grab user input
+			std::string input;
+			// Grab user input for other functions (volume, kick, leave)
+			getline(cin,input);
+
+			if(input == "-q")
+			{
+				pchat.network.disconnect();
+				break;
+			}
+
+			if(input == "-k") // -k (user)
+			{
+				// Kick someone
+			}
+
+			if(input == "-v") // -v (+/- num)
+			{
+				//set volume
+				pchat.audio.setOutputVolume((float) input); // still needs string parsing
+			}
+
+			if(input == "-m") // -m (user)
+			{
+				//mute someone
+				pchat.audio.setMuteMic(true);
+			}
 
 		}
 	}
 	else
 	{
-		std::cout << "GUI mode" << std::endl;
+		// std::cout << "GUI mode" << std::endl;
 		peer.GUI.runGui(argc,argv);
 	}
 
