e3d2673c46e1a251baf9662bee75fd936e4a198b
Omar Ahmadyar <oahmadyar@gmail.com>
2020-07-30 21:38:57 -0700

    Updated Documentation and Fixed Small Things
    Fixes:
    --> Updated AudioPacket buffer to type of array pointer instead of just pointer
    --> Added use of make_unique
    --> out_packet_id now starts at 1 instead of 0
          - value of 0 could cause packet loss count of -1 after first packet,
            furthermore if packet loss count is represented by unsigned number
            then it would result in max value
    --> Privated useless NPeer constructor
    --> Deleted duplicate myName member from PeersChatNetwork

--- src/Network/PC_Network.hpp
+++ src/Network/PC_Network.hpp
@@ -122,14 +122,14 @@ static inline int close(int &x) { return closesocket(x); }
  * add too much latency.
  *
  * SOCKET_TIMEOUT adds a cap to sockets so that they don't waste time on dead peers.
- * Make this too short and you might now give your peers enough time to respond.  Make
+ * Make this too short and you might not give your peers enough time to respond.  Make
  * this too long and the program will take longer to close and dead connections will
  * stick around longer.
  *
  * PEER_TIMEOUT is a duration of time that if you haven't received audio from a peer
  * for this long, then the peer will be disconnected.
  *
- * PORT is the port that this program will be using
+ * PORT is the TCP/UDP port that this program will be using
  *
 */
 extern std::chrono::milliseconds PACKET_DELAY;
@@ -144,27 +144,21 @@ extern uint16_t PORT;
 /* AudioPacket: A struct that contains an encoded audio packet
  *
  * @member packet_id  Unique id for an incoming packet from this peer.  Packets
- *                    received will be numbered sequentially starting with zero
+ *                    received will be numbered sequentially starting with one
  *
  * @member packet_len The size of packet in bytes
  *
  * @member packet  A unique pointer to a buffer meant for containing an encoded
  *                 audio packet from libopus
  *
- * @constructor AudioPacket()  Initializes packet to at least BUFFER_SIZE which
- *                             is always at least 4096
- *
- * @constructor AudioPacket(2)  Same as @AudioPacket() but allows you to
- *                              populate @packet and set @packet_len.
- *
  * @operator <  Less than operator overload for comparing and sorting based
  *              packet id number.
  */
 struct AudioPacket
 {
-	uint32_t packet_id = 0;
+	uint32_t packet_id  = 0;
 	uint16_t packet_len = 0;
-	std::unique_ptr<uint8_t> packet = std::unique_ptr<uint8_t>(new uint8_t[BUFFER_SIZE]);
+	std::unique_ptr<uint8_t[]> packet = std::make_unique<uint8_t[]>(BUFFER_SIZE);
 	inline bool operator<(const AudioPacket &other) { return this->packet_id < other.packet_id; }
 };
 
@@ -183,8 +177,47 @@ inline bool AudioInPacket_greater(const std::unique_ptr<AudioInPacket> &left, co
 // NPeer Class ---------------------------------------------------------------------------
 /* NPeer: A class for handling networking to your peers -- API DOCUMENTATION
  *
- * @constructor NPeer(0)  Default constructor
+(IMPLEMENTATION DETAILS)
+Members:
+ * tcp  TCP socket to connect to this NPeer to ask some sort of request
  *
+ * udp  (static) UDP socket to send audio to Peers
+ *
+ * destination  Address to this Peer
+ *
+ * pname  Peer Name
+ *
+ * ID  Peer ID num
+ *
+ * muted  Are we ignoring/muting the peer?
+ *
+ * in_packets  Priority Queue of packets that are received from this peer
+ *
+ * in_queue_lock  Lock on the above for atomicity
+ *
+ * in_bucket  bucket where requested AudioInPackets are retrieved from
+ *
+ * in_bucket_lock  Lock on the above for atomicity
+ *
+ * in_packet_id  The id of the last packet that was returned by getAudioInPacket()
+ *
+ * out_packets  Queue of packets that are going to be sent out over network
+ *
+ * out_queue_lock  Lock on the above
+ *
+ * out_bucket  Bucket where requested AudioOutPackets are retrieved from
+ *
+ * out_bucket_lock  Lock on the above
+ *
+ * out_packet_id  The id to stamp onto the next AudioOutPacket passed to @enqueue_out
+ *
+ * run_thread  Is this class running?  AKA are we receiving audio and listening on tcp?
+ *
+ * out_packet_count  The number of AudioOutPacket objects that are queue'd out.
+ *
+ *
+(CLIENT INTERFACE)
+Constructors:
  * @constructor NPeer(2)  Constructor that initializes address structure for peer
  *                        destination.
  *                      @param ip: (const char*)  IP address in standard string format
@@ -193,10 +226,29 @@ inline bool AudioInPacket_greater(const std::unique_ptr<AudioInPacket> &left, co
  *                              byte order
  *                              Ex: 8080
  *
+ * @constructor NPeer(1)  Same as above but if you already created a sockaddr
+ *
+ *
+Public Methods:
+ * @method getName()  Method that returns the NPeer's name
+ *                   @return std::string
+ *
+ * @method setName(1)  Method that sets the NPeer's name
+ *                    @param name: (std::string) name to set
+ *                    @return (bool) did it succeed?
+ *
+ * @method getID()  Method that returns the Peer's unique ID to you
+ *
+ * @method setMute(1)  Method that sets whether you want to mute or unmute this peer
+ *                    @param x (bool) do you want to mute them?
+ *
+ * @method getMute()  Method that tells you if the peer is muted or not
+ *                   @return (bool) are they muted?
+ *
  * @method getEmptyOutPacket()  Method that returns an @AudioOutPacket.  Packet may have
- *                              junk/old data in it, make sure to overwrite.
- *                              @AudioOutPacket returned should be passed to @enqueue_out
- *                              after being populated with audio packet data.
+ *                              junk/old data in it.  @AudioOutPacket returned should be
+ *                              passed to @enqueue_out after being populated with audio
+ *                              packet data.
  *                            @return (AudioOutPacket*) Pointer to AudioOutPacket that is
  *                              given to the client to be populated with audio/mic data.
  *
@@ -205,9 +257,12 @@ inline bool AudioInPacket_greater(const std::unique_ptr<AudioInPacket> &left, co
  *                       @param packet: (AudioOutPacket*)  A pointer to an AudioOutPacket
  *                               that is populated with audio data from client.
  *
+ * @method startNetStream()  Method that starts thread to output audio on @udp to peer
+ *
+ * @method stopNetStream()  Method that stops thread outputting audio on @udp to peer
+ *
  * @method getEmptyInPacket()  Method that returns a @AudioInPacket.  Packet may contain
- *                             junk data that should be overwritten.  Should be populated
- *                             with mic data from peer.
+ *                             junk data.  Should be populated with opus data from peer.
  *                           @return (AudioInPacket*) Pointer to AudioInPacket that is
  *                            given to the client to be populated with audio from peer.
  *
@@ -235,10 +290,12 @@ inline bool AudioInPacket_greater(const std::unique_ptr<AudioInPacket> &left, co
  *                          packet loss, and if there is packet loss, the number of
  *                          lost packets will be the difference minus 1.
  *                        @return (uint32_t) Last packet id returned by @getAudioInPacket
- *                          Ex: uint32_t lastId = getInPacketId();
+ *                          Ex: auto lastId = getInPacketId();
  *                              AudioInPacket *packet = getAudioInPacket();
- *                              int packets_lost = packet->packet_id - lastId - 1;
+ *                              auto packets_lost = packet->packet_id - lastId - 1;
  *
+ *
+Operators:
  * @operator ==(1)  Equivalence operator.  Compare an address to see if this NPeer routes
  *                  to that address.
  *                 @param addr (sockaddr_in)  Adress you want to check equivalence with
@@ -270,13 +327,14 @@ private:
 	std::mutex out_queue_lock;
 	std::queue<std::unique_ptr<AudioOutPacket>> out_bucket;
 	std::mutex out_bucket_lock;
-	uint32_t out_packet_id = 0;
+	uint32_t out_packet_id = 1;
 	bool run_thread = false;
 	std::atomic<int> out_packet_count = {0};
 
 	// Constructor
-public:
+private:
 	NPeer() noexcept;
+public:
 	NPeer(const char* ip, const uint16_t &port);
 	NPeer(const sockaddr_in &addr) noexcept;
 	~NPeer() noexcept;
@@ -323,20 +381,43 @@ private:
 };
 
 
+/* NPeerAttorney: NPeer's Lawyer
+ *
+ * Allows PeersChatNetwork to access some private data/methods/functions from NPeer
+ * without having total access.  Acts as an intermediary so that PeersChatNetwork
+ * has to go through NPeerAttorney's API to access restricted NPeer API.
+ *
+ *
+ * No Documentation since this class has no public API and is extremely simple anyways.
+ */
 class NPeerAttorney
 {
 	static inline bool createTCP(NPeer *peer) {
 		return peer->createTCP();
 	}
+
 	static inline void destroyTCP(NPeer *peer) {
 		peer->destroyTCP();
 	}
+
 	static inline sockaddr_in getDest(NPeer *peer) {
 		return peer->getDest();
 	}
-	static inline int getUDP() { return NPeer::udp; }
-	static inline void destroyUDP() { if(NPeer::udp > 0) close(NPeer::udp); NPeer::udp = -1; }
-	static inline int getTCP(NPeer *peer) { return peer->tcp; }
+
+	static inline int getUDP() {
+		return NPeer::udp;
+	}
+
+	static inline void destroyUDP() {
+		if(NPeer::udp > 0) close(NPeer::udp);
+		NPeer::udp = -1;
+	}
+
+	static inline int getTCP(NPeer *peer) {
+		return peer->tcp;
+	}
+
+
 	friend class PeersChatNetwork;
 };
 
@@ -344,19 +425,32 @@ class NPeerAttorney
 // PeersChatNetwork Class ----------------------------------------------------------------
 /* PeersChatNetwork: A Class that handles all your PeersChat networking needs
  *
+(IMPLEMENTATION DETAILS)
 Members:
+ * myName  My (the client's) name
+ *
  * peers  Vector of NPeers that we are in a call with
  *
+ * peers_lock  mutex on @peers
+ *
+ * size  Number of people in the call
+ *
  * tcp_listen  Socket we are listening for tcp requests on
+ *
  * accept_direct_join  Flag that indicates whether we are going to allow people to join
  *                     the call through this client.
  *
  * accept_indirect_join  Flag that indicates whether we are going to allow anybody to join
- *                       the call at all
+ *                       the call through another peer
  *
  * running  Flag that indicates whether PeersChatNetwork is currently running
  *
+ * listen_thread  Thread that listens for tcp requests (CONNECT/REQN/etc)
  *
+ * recv_thread  Thread that receives audio from peers and sorts to respective NPeer
+ *
+ *
+(CLIENT INTERFACE)
 Constructors:
  * PeersChatNetwork()  Create PeersChatNetwork Object
  * ~PeersChatNetwork()  Destroy PeersChatNetwork Object
@@ -364,52 +458,49 @@ Constructors:
 Operators:
  * operator[sockaddr_in]  Return the a pointer to the NPeer object corresonpding to a
  *                        specific destination address
+ *                       @return NPeer* (non owning)
  *
  * operator[int]  Return the pointer to the Npeer object stored at position x.  Useful
  *                for looping through all NPeer objects.
+ *               @return NPeer* (non owning)
  *
+ * operator[std::string]  Return the pointer to the NPeer object with name x if they are
+ *                        the only one with that name
+ *                       @return NPeer* (non owning)
  *
 Public Methods:
+ * findID(int)  Get a Peer by their ID number
+ *             @return NPeer* (non owning)
+ *
+ * getMyName()  Get my (the client's) name
+ *             @return (std::string) name
+ *
+ * setMyName(std::string)  Set my (the client's) name
+ *                        @return (bool) success?
+ *
  * join(sockaddr_in)  Join a PeersChat session using a sockaddr_in struct as the dest
+ *                   @return (bool) success?
+ *
+ * getNames()  Request name from every NPeer
  *
  * host()  Host your own PeersChat session
+ *        @return (bool) success?
  *
- * start()  Start Receiving Audio and listening for connections
+ * disconnect()  Leave the current call
  *
- * stop()  Stop Receiving Audio and listening for connections
+ * getNumberPeers()  Get number of peers.  Useful for looping over them.
+ *                  @return (int) number of peers
  *
- * getNumberPeers()  Get number of other peers.  Useful for looping over them.
+ * setIndirectJoin(bool)  Allow or disallow indirect joins
  *
- *
-Private Methods:
- * propose(sockaddr_in, int)  Ask currently connected peer(int) if person represented
- *                            by sockaddr_in can join
- *
- * respond(bool, int)  ACCEPT/DECLINE currently connected peer's(int) last statement
- *
- * getResponse(int)  Find out if they accepted or declined your request
- *
- * addPeer(sockaddr_in)  Add new member to your PeersChatNetwork as an NPeer
- *
- * requestPeers(int, std::vector<sockaddr_in>)  Ask for peer's entire peer list
- *
- * sendPeers(int) Send all your peers to peer at socket(int)
- *
- * connect(int)  Ask to connect to client at socket(int)
- *
- * disconnect(int)  Tell peer at socket(int) you are disconnecting
- *
- * receive_audio_thread  Function to start in its own thread to receive audio
- *
- * listen_on_tcp_thread  Function to start in its own thread to listen for any
- *                       tcp connections/requests.
+ * setDirectJoin(bool)  Allow or disallow direct joins
  *
  */
 class PeersChatNetwork
 {
 	// Members
 private:
-	char myname[MAX_NAME_LEN+1] = {0};
+	char myName[MAX_NAME_LEN+1] = {0};
 	std::vector<std::unique_ptr<NPeer>> peers;
 	std::mutex peers_lock;
 	int size = 0;
@@ -419,7 +510,6 @@ private:
 	bool running = false;
 	std::unique_ptr<std::thread> listen_thread;
 	std::unique_ptr<std::thread> recv_thread;
-	char myName[MAX_NAME_LEN + 1] = {0};
 
 public:
 	PeersChatNetwork();
